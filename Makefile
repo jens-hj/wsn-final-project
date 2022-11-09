e7120317beba485a09603b636b942931fc6bd9d5
Jens <jens.jens@live.dk>
2022-11-09 15:27:22 +0100

    feat(aes): print in hex, added energest module

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 CONTIKI_PROJECT = aes
 all: $(CONTIKI_PROJECT)
 
+# Add library flags
 # TARGET_LIBFILES += -lm
+
+# Add project modules
+# MODULES += os/services/simple-energest
 
 CONTIKI = ../..
 include $(CONTIKI)/Makefile.include
--- aes.c
+++ aes.c
@@ -15,33 +15,42 @@ PROCESS_THREAD(aes_process, ev, data) {
   cc2420_on();
 
   // sequence to be encrypted
-  uint8_t vals[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  unsigned char plaintext[AES_128_BLOCK_SIZE] = "this is a test 1";
   // char* str = "This is a test";
+
+  // for (int i = 0; i < 16; i++) {
+  //   printf("%d ", (char)plaintext[i]);
+  // }
+  // printf("\n");
+  printf("plaintext: %s\n", plaintext);
 
   // 128 bit key
   // user defined key
   // int key_length = 16;
-  uint8_t key[16] = {5, 0, 7, 6, 9, 9, 6, 2, 9, 1, 3, 8, 6, 8, 4, 0};
+  uint8_t key[AES_128_KEY_LENGTH] = {5, 0, 7, 6, 9, 9, 6, 2, 9, 1, 3, 8, 6, 8, 4, 0};
 
   // cc2420_aes_set_key(key, 0);
-  // cc2420_aes_cipher(vals, N, 0);
+  // cc2420_aes_cipher(plaintext, N, 0);
 
   AES_128.set_key(key);
-  AES_128.encrypt(vals);
+  AES_128.encrypt(plaintext);
+  printf("encrypted: %s\n", plaintext);
 
-  for (int i = 0; i < 16; i++) {
-    printf("%d ", vals[i]);
+  printf("encrypted: ");
+  for (int i = 0; i < AES_128_BLOCK_SIZE; i++) {
+  printf("%02x ", plaintext[i]);
   }
   printf("\n");
 
   // decrypt
-  AES_128.encrypt(vals);
+  AES_128.encrypt(plaintext);
+  printf("decrypted: %s\n", plaintext);
 
   // print decrypted
-  for (int i = 0; i < 16; i++) {
-    printf("%d ", vals[i]);
-  }
-  printf("\n");
+  // for (int i = 0; i < 16; i++) {
+  // printf("%c ", (char)plaintext[i]);
+  // }
+  // printf("\n");
 
   cc2420_off();
   
