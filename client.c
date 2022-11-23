7608f56a3663e7e58badfe4e8b5c7206691f300a
Pernille <pernillepallesen3@gmail.com>
2022-11-23 14:12:37 +0100

    feat(client): small change

--- client.c
+++ client.c
@@ -99,6 +99,10 @@ PROCESS_THREAD(udp_client_process, ev, data)
       
       for (int i = 0; i < AES_128_BLOCK_SIZE; i++) {
         light_data[i] = light_sensor.value(LIGHT_SENSOR_TOTAL_SOLAR);
+        int n = 0;
+        while(n < 1000) {
+          n++;
+        }
         //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
         //etimer_reset(&timer);
       }
