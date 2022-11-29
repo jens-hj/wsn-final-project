25e5d8af9889cd3419686a89d84e0cd6814a4c3c
LAPTOP-IG6GC8VB\anisa <anisa-m@hotmail.com>
2022-11-29 15:50:54 +0100

    fix(client): light sensor waits

---
+++ client.c
@@ -0,0 +1,144 @@
+#include "contiki.h"
+#include "net/routing/routing.h"
+#include "random.h"
+#include "net/netstack.h"
+#include "net/ipv6/simple-udp.h"
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+#include "lib/aes-128.h"
+#include "dev/radio.h"
+#include "cc2420.h"
+#include "dev/light-sensor.h"
+
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+#define WITH_SERVER_REPLY  1
+#define UDP_CLIENT_PORT	8765
+#define UDP_SERVER_PORT	5678
+
+#define SEND_INTERVAL		  (5 * CLOCK_SECOND)
+
+static struct simple_udp_connection udp_conn;
+static uint32_t rx_count = 0;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(udp_client_process, "UDP client");
+AUTOSTART_PROCESSES(&udp_client_process);
+/*---------------------------------------------------------------------------*/
+static void
+udp_rx_callback(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
+
+  LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
+  LOG_INFO_6ADDR(sender_addr);
+#if LLSEC802154_CONF_ENABLED
+  LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
+#endif
+  LOG_INFO_("\n");
+  rx_count++;
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udp_client_process, ev, data)
+{
+  static struct etimer periodic_timer;
+  //static char str[32];
+  uip_ipaddr_t dest_ipaddr;
+  static uint32_t tx_count;
+  static uint32_t missed_tx_count;
+
+  uint8_t key[AES_128_KEY_LENGTH] = {5, 0, 7, 6, 9, 9, 6, 2, 9, 1, 3, 8, 6, 8, 4, 0};
+  uint8_t light_data[AES_128_BLOCK_SIZE];
+  static struct etimer timer;
+
+
+  PROCESS_BEGIN();
+  etimer_set(&timer, CLOCK_SECOND * 0.1);
+
+
+  AES_128.set_key(key);
+
+  /* Initialize UDP connection */
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
+                      UDP_SERVER_PORT, udp_rx_callback);
+
+  etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
+  while(1) {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+
+    if(NETSTACK_ROUTING.node_is_reachable() &&
+        NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
+
+      /* Print statistics every 10th TX */
+      if(tx_count % 10 == 0) {
+        LOG_INFO("Tx/Rx/MissedTx: %" PRIu32 "/%" PRIu32 "/%" PRIu32 "\n",
+                 tx_count, rx_count, missed_tx_count);
+      }
+      
+      SENSORS_ACTIVATE(light_sensor); // ACTIVATING LIGHT SENSOR
+
+
+      /*
+      int i = 0;
+      while (i < AES_128_BLOCK_SIZE) {
+        light_data[i] = light_sensor.value(LIGHT_SENSOR_TOTAL_SOLAR);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+        etimer_reset(&timer);
+        i++;
+      }
+      */
+
+      static int i;
+
+      for (i = 0; i < AES_128_BLOCK_SIZE; i++) {
+        light_data[i] = light_sensor.value(LIGHT_SENSOR_TOTAL_SOLAR);
+        //etimer_reset(&timer);
+        //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+
+      }
+      
+
+      LOG_INFO("Light Sensor Data: ");
+      for (int j = 0; j < AES_128_BLOCK_SIZE; j++) {
+
+        LOG_INFO_("%d ", light_data[j]);
+      }
+      LOG_INFO_("\n");
+
+      SENSORS_DEACTIVATE(light_sensor);
+
+      AES_128.encrypt(light_data);
+
+      /* Send to DAG root */
+      LOG_INFO("Sending request %"PRIu32" to ", tx_count);
+      LOG_INFO_6ADDR(&dest_ipaddr);
+      LOG_INFO_("\n");
+      //snprintf(str, sizeof(str), "hello %" PRIu32 "", tx_count);
+      //snprintf(str, sizeof(str), "%s\n", message);
+      //simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
+      simple_udp_sendto(&udp_conn, light_data, sizeof(light_data)/sizeof(light_data[0]), &dest_ipaddr);
+      tx_count++;
+    } else {
+      LOG_INFO("Not reachable yet\n");
+      if(tx_count > 0) {
+        missed_tx_count++;
+      }
+    }
+
+    /* Add some jitter */
+    etimer_set(&periodic_timer, SEND_INTERVAL
+      - CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)));
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
